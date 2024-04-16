03bc4cd1037bd83b96915e0aca5ef4ab1643279b
Sonia Zhang <skzhang@usc.edu>
2024-04-16 10:15:13 -0700

    compiles, very basic game display

--- vga_bitchange.v
+++ vga_bitchange.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date:    12:15:38 12/14/2017 
+// Create Date:  12:15:38 12/14/2017 
 // Design Name: 
-// Module Name:    vgaBitChange 
+// Module Name:  vgaBitChange 
 // Project Name: 
 // Target Devices: 
 // Tool versions: 
@@ -21,73 +21,236 @@
 // Author: Yue (Julien) Niu
 // Description: Port from NEXYS3 to NEXYS4
 //////////////////////////////////////////////////////////////////////////////////
+
+// if cell & arr, color = light grey
+// else if within rectangle = grey
+// else background = black
+
 module vga_bitchange(
-	input clk,
-	input bright,
-	input button,
+	input Clk,
+	input [9:0][11:0] arr,
 	input [9:0] hCount, vCount,
-	output reg [11:0] rgb,
-	output reg [15:0] score
-   );
+	output reg [11:0] rgb);
 	
 	parameter BLACK = 12'b0000_0000_0000;
-	parameter WHITE = 12'b1111_1111_1111;
-	parameter RED   = 12'b1111_0000_0000;
-	parameter GREEN = 12'b0000_1111_0000;
-	//parameter BLUE = 12'b0000_0000_1111;
+	parameter GREY = 12'b0011_0011_0011;
+	parameter LIGHT_GREY = 12'b1010_1010_1010;
 
-	wire whiteZone;
-	wire greenMiddleSquare;
-	reg reset;
-	reg[9:0] greenMiddleSquareY;
-	reg[49:0] greenMiddleSquareSpeed; 
-
-	initial begin
-		greenMiddleSquareY = 10'd320;
-		score = 15'd0;
-		reset = 1'b0;
-	end
+	wire [9:0][11:0] squares;
+	wire rectangle;
 	
 	
 	always@ (*) // paint a white box on a red background
-    	if (~bright)
-		rgb = BLACK; // force black if not bright
-	 else if (greenMiddleSquare == 1)
-		rgb = GREEN;
-	 else if (whiteZone == 1)
-		rgb = WHITE; // white box
-	 else
-		rgb = RED; // background color
+	begin
+		if (squares[0][0]  && arr[0][0])
+			rgb = LIGHT_GREY;
+		else if (squares[0][1]  && arr[0][1])
+			rgb = LIGHT_GREY;
+		else if (squares[0][2]  && arr[0][2])
+			rgb = LIGHT_GREY;
+		else if (squares[0][3]  && arr[0][3])
+			rgb = LIGHT_GREY;
+		else if (squares[0][4]  && arr[0][4])
+			rgb = LIGHT_GREY;
+		else if (squares[0][5]  && arr[0][5])
+			rgb = LIGHT_GREY;
+		else if (squares[0][6]  && arr[0][6])
+			rgb = LIGHT_GREY;
+		else if (squares[0][7]  && arr[0][7])
+			rgb = LIGHT_GREY;
+		else if (squares[0][8]  && arr[0][8])
+			rgb = LIGHT_GREY;
+		else if (squares[0][9]  && arr[0][9])
+			rgb = LIGHT_GREY;
+		else if (squares[0][10]  && arr[0][10])
+			rgb = LIGHT_GREY;
+		else if (squares[0][11]  && arr[0][11])
+			rgb = LIGHT_GREY;
+		else if (squares[1][0]  && arr[1][0])
+			rgb = LIGHT_GREY;
+		else if (squares[1][1]  && arr[1][1])
+			rgb = LIGHT_GREY;
+		else if (squares[1][2]  && arr[1][2])
+			rgb = LIGHT_GREY;
+		else if (squares[1][3]  && arr[1][3])
+			rgb = LIGHT_GREY;
+		else if (squares[1][4]  && arr[1][4])
+			rgb = LIGHT_GREY;
+		else if (squares[1][5]  && arr[1][5])
+			rgb = LIGHT_GREY;
+		else if (squares[1][6]  && arr[1][6])
+			rgb = LIGHT_GREY;
+		else if (squares[1][7]  && arr[1][7])
+			rgb = LIGHT_GREY;
+		else if (squares[1][8]  && arr[1][8])
+			rgb = LIGHT_GREY;
+		else if (squares[1][9]  && arr[1][9])
+			rgb = LIGHT_GREY;
+		else if (squares[1][10]  && arr[1][10])
+			rgb = LIGHT_GREY;
+		else if (squares[1][11]  && arr[1][11])
+			rgb = LIGHT_GREY;
+		else if (squares[2][0]  && arr[2][0])
+			rgb = LIGHT_GREY;
+		else if (squares[2][1]  && arr[2][1])
+			rgb = LIGHT_GREY;
+		else if (squares[2][2]  && arr[2][2])
+			rgb = LIGHT_GREY;
+		else if (squares[2][3]  && arr[2][3])
+			rgb = LIGHT_GREY;
+		else if (squares[2][4]  && arr[2][4])
+			rgb = LIGHT_GREY;
+		else if (squares[2][5]  && arr[2][5])
+			rgb = LIGHT_GREY;
+		else if (squares[2][6]  && arr[2][6])
+			rgb = LIGHT_GREY;
+		else if (squares[2][7]  && arr[2][7])
+			rgb = LIGHT_GREY;
+		else if (squares[2][8]  && arr[2][8])
+			rgb = LIGHT_GREY;
+		else if (squares[2][9]  && arr[2][9])
+			rgb = LIGHT_GREY;
+		else if (squares[2][10]  && arr[2][10])
+			rgb = LIGHT_GREY;
+		else if (squares[2][11]  && arr[2][11])
+			rgb = LIGHT_GREY;
+		else if (squares[3][0]  && arr[3][0])
+			rgb = LIGHT_GREY;
+		else if (squares[3][1]  && arr[3][1])
+			rgb = LIGHT_GREY;
+		else if (squares[3][2]  && arr[3][2])
+			rgb = LIGHT_GREY;
+		else if (squares[3][3]  && arr[3][3])
+			rgb = LIGHT_GREY;
+		else if (squares[3][4]  && arr[3][4])
+			rgb = LIGHT_GREY;
+		else if (squares[3][5]  && arr[3][5])
+			rgb = LIGHT_GREY;
+		else if (squares[3][6]  && arr[3][6])
+			rgb = LIGHT_GREY;
+		else if (squares[3][7]  && arr[3][7])
+			rgb = LIGHT_GREY;
+		else if (squares[3][8]  && arr[3][8])
+			rgb = LIGHT_GREY;
+		else if (squares[3][9]  && arr[3][9])
+			rgb = LIGHT_GREY;
+		else if (squares[3][10]  && arr[3][10])
+			rgb = LIGHT_GREY;
+		else if (squares[3][11]  && arr[3][11])
+			rgb = LIGHT_GREY;
+		else if (squares[4][0]  && arr[4][0])
+			rgb = LIGHT_GREY;
+		else if (squares[4][1]  && arr[4][1])
+			rgb = LIGHT_GREY;
+		else if (squares[4][2]  && arr[4][2])
+			rgb = LIGHT_GREY;
+		else if (squares[4][3]  && arr[4][3])
+			rgb = LIGHT_GREY;
+		else if (squares[4][4]  && arr[4][4])
+			rgb = LIGHT_GREY;
+		else if (squares[4][5]  && arr[4][5])
+			rgb = LIGHT_GREY;
+		else if (squares[4][6]  && arr[4][6])
+			rgb = LIGHT_GREY;
+		else if (squares[4][7]  && arr[4][7])
+			rgb = LIGHT_GREY;
+		else if (squares[4][8]  && arr[4][8])
+			rgb = LIGHT_GREY;
+		else if (squares[4][9]  && arr[4][9])
+			rgb = LIGHT_GREY;
+		else if (squares[4][10]  && arr[4][10])
+			rgb = LIGHT_GREY;
+		else if (squares[4][11]  && arr[4][11])
+			rgb = LIGHT_GREY;
+		else if (squares[5][0]  && arr[5][0])
+			rgb = LIGHT_GREY;
+		else if (squares[5][1]  && arr[5][1])
+			rgb = LIGHT_GREY;
+		else if (squares[5][2]  && arr[5][2])
+			rgb = LIGHT_GREY;
+		else if (squares[5][3]  && arr[5][3])
+			rgb = LIGHT_GREY;
+		else if (squares[5][4]  && arr[5][4])
+			rgb = LIGHT_GREY;
+		else if (squares[5][5]  && arr[5][5])
+			rgb = LIGHT_GREY;
+		else if (squares[5][6]  && arr[5][6])
+			rgb = LIGHT_GREY;
+		else if (squares[5][7]  && arr[5][7])
+			rgb = LIGHT_GREY;
+		else if (squares[5][8]  && arr[5][8])
+			rgb = LIGHT_GREY;
+		else if (squares[5][9]  && arr[5][9])
+			rgb = LIGHT_GREY;
+		else if (squares[5][10]  && arr[5][10])
+			rgb = LIGHT_GREY;
+		else if (squares[5][11]  && arr[5][11])
+			rgb = LIGHT_GREY;
+		else if (squares[6][0]  && arr[6][0])
+			rgb = LIGHT_GREY;
+		else if (squares[6][1]  && arr[6][1])
+			rgb = LIGHT_GREY;
+		else if (squares[6][2]  && arr[6][2])
+			rgb = LIGHT_GREY;
+		else if (squares[6][3]  && arr[6][3])
+			rgb = LIGHT_GREY;
+		else if (squares[6][4]  && arr[6][4])
+			rgb = LIGHT_GREY;
+		else if (squares[6][5]  && arr[6][5])
+			rgb = LIGHT_GREY;
+		else if (squares[6][6]  && arr[6][6])
+			rgb = LIGHT_GREY;
+		else if (squares[6][7]  && arr[6][7])
+			rgb = LIGHT_GREY;
+		else if (squares[6][8]  && arr[6][8])
+			rgb = LIGHT_GREY;
+		else if (squares[6][9]  && arr[6][9])
+			rgb = LIGHT_GREY;
+		else if (squares[6][10]  && arr[6][10])
+			rgb = LIGHT_GREY;
+		else if (squares[6][11]  && arr[6][11])
+			rgb = LIGHT_GREY;
+		else if (squares[7][0]  && arr[7][0])
+			rgb = LIGHT_GREY;
+		else if (squares[7][1]  && arr[7][1])
+			rgb = LIGHT_GREY;
+		else if (squares[7][2]  && arr[7][2])
+			rgb = LIGHT_GREY;
+		else if (squares[7][3]  && arr[7][3])
+			rgb = LIGHT_GREY;
+		else if (squares[7][4]  && arr[7][4])
+			rgb = LIGHT_GREY;
+		else if (squares[7][5]  && arr[7][5])
+			rgb = LIGHT_GREY;
+		else if (squares[7][6]  && arr[7][6])
+			rgb = LIGHT_GREY;
+		else if (squares[7][7]  && arr[7][7])
+			rgb = LIGHT_GREY;
+		else if (squares[7][8]  && arr[7][8])
+			rgb = LIGHT_GREY;
+		else if (squares[7][9]  && arr[7][9])
+			rgb = LIGHT_GREY;
+		else if (squares[7][10]  && arr[7][10])
+			rgb = LIGHT_GREY;
+		else if (squares[7][11]  && arr[7][11])
+			rgb = LIGHT_GREY;
+		else if (rectangle)
+			rgb = GREY; // game board
+		else
+			rgb = BLACK; // background color
+	end
 
-	
-	always@ (posedge clk)
+	assign rectangle = ((hCount >= 10'd99) && (hCount <= 10'd369)) && ((vCount >= 10'd77) && (vCount <= 10'd401)) ? 1 : 0;
+
+	genvar i, j;
+	generate
+		for(i = 0; i < 10; i = i + 1)
 		begin
-		greenMiddleSquareSpeed = greenMiddleSquareSpeed + 50'd1;
-		if (greenMiddleSquareSpeed >= 50'd500000) //500 thousand
-			begin
-			greenMiddleSquareY = greenMiddleSquareY + 10'd1;
-			greenMiddleSquareSpeed = 50'd0;
-			if (greenMiddleSquareY == 10'd779)
-				begin
-				greenMiddleSquareY = 10'd0;
-				end
+			for (j = 0; i < 12; j = j + 1)
+			begin 
+				assign squares[i][j] = ((hCount >= 10'd100 + 10'd25 * i) && (hCount <= 10'd125 + 10'd25 * i)) && ((vCount >= 10'd78 + 10'd25 * j) && (vCount <= 10'd103 + 10'd25 * j)) ? 1 : 0;	
 			end
 		end
+	endgenerate
 
-	always@ (posedge clk)
-		if ((reset == 1'b0) && (button == 1'b1) && (hCount >= 10'd144) && (hCount <= 10'd784) && (greenMiddleSquareY >= 10'd400) && (greenMiddleSquareY <= 10'd475))
-			begin
-			score = score + 16'd1;
-			reset = 1'b1;
-			end
-		else if (greenMiddleSquareY <= 10'd20)
-			begin
-			reset = 1'b0;
-			end
-
-	assign whiteZone = ((hCount >= 10'd144) && (hCount <= 10'd784)) && ((vCount >= 10'd400) && (vCount <= 10'd475)) ? 1 : 0;
-
-	assign greenMiddleSquare = ((hCount >= 10'd340) && (hCount < 10'd380)) &&
-				   ((vCount >= greenMiddleSquareY) && (vCount <= greenMiddleSquareY + 10'd40)) ? 1 : 0;
-	
 endmodule
--- vga_top.v
+++ vga_top.v
@@ -23,40 +23,126 @@
 //////////////////////////////////////////////////////////////////////////////////
 module vga_top(
 	input ClkPort,
-	input BtnC,
-	input BtnU,
-	
+	input BtnL, BtnU, BtnD, BtnR, BtnC,	
+
 	//VGA signal
 	output hSync, vSync,
 	output [3:0] vgaR, vgaG, vgaB,
 	
-	//SSG signal 
-	output An0, An1, An2, An3, An4, An5, An6, An7,
-	output Ca, Cb, Cc, Cd, Ce, Cf, Cg, Dp,
+	// //SSG signal 
+	// output An0, An1, An2, An3, An4, An5, An6, An7,
+	// output Ca, Cb, Cc, Cd, Ce, Cf, Cg, Dp,
 	
 	output MemOE, MemWR, RamCS, QuadSpiFlashCS
 	);
 	
+	// CLOCKING 
+
+	wire		Reset;
+	wire		board_clk, game_clk;
+	reg [26:0]	DIV_CLK;
+
+	BUFGP BUFGP1 (board_clk, ClkPort);
+
+	always @(posedge board_clk, posedge Reset) 	
+    begin							
+        if (Reset)
+		DIV_CLK <= 0;
+        else
+		DIV_CLK <= DIV_CLK + 1'b1;
+    end
+
+	assign game_clk = DIV_CLK[20];
+
+
+	// disable mamory ports
+	assign {MemOE, MemWR, RamCS, QuadSpiFlashCS} = 4'b1111;
+
 	wire bright;
 	wire[9:0] hc, vc;
 	wire[15:0] score;
-	wire [6:0] ssdOut;
-	wire [3:0] anode;
+	// wire [6:0] ssdOut;
+	// wire [3:0] anode;
 	wire [11:0] rgb;
+
+	wire Start_Ack;
+	wire BtnR_pulse, BtnL_pulse, BtnD_pulse, BtnU_pulse;
+	wire bottom_flag, top_flag, gen_flag; 
+	assign Reset = BtnC;
+	assign Start_Ack = BtnU;
+
+	wire x1,y1,x2,y2,x3,y3,x4,y4;
+	reg [9:0][11:0] arr;
+
 	display_controller dc(.clk(ClkPort), .hSync(hSync), .vSync(vSync), .bright(bright), .hCount(hc), .vCount(vc));
-	vga_bitchange vbc(.clk(ClkPort), .bright(bright), .button(BtnU), .hCount(hc), .vCount(vc), .rgb(rgb), .score(score));
-	counter cnt(.clk(ClkPort), .displayNumber(score), .anode(anode), .ssdOut(ssdOut));
+	vga_bitchange vbc(.Clk(ClkPort), .hCount(hc), .vCount(vc), .arr(arr), .rgb(rgb));
+	// counter cnt(.clk(ClkPort), .displayNumber(score), .anode(anode), .ssdOut(ssdOut));
 	
-	assign Dp = 1;
-	assign {Ca, Cb, Cc, Cd, Ce, Cf, Cg} = ssdOut[6 : 0];
-    assign {An7, An6, An5, An4, An3, An2, An1, An0} = {4'b1111, anode};
+	// assign Dp = 1;
+	// assign {Ca, Cb, Cc, Cd, Ce, Cf, Cg} = ssdOut[6 : 0];
+    // assign {An7, An6, An5, An4, An3, An2, An1, An0} = {4'b1111, anode};
 
 	
 	assign vgaR = rgb[11 : 8];
 	assign vgaG = rgb[7  : 4];
 	assign vgaB = rgb[3  : 0];
+
+	ee354_debouncer #(.N_dc(28)) button_U 
+        (.CLK(sys_clk), .RESET(Reset), .PB(BtnU), .DPB( ), 
+		.SCEN(BtnU_pulse), .MCEN( ), .CCEN( ));
+
+	ee354_debouncer #(.N_dc(28)) button_D 
+        (.CLK(sys_clk), .RESET(Reset), .PB(BtnD), .DPB( ), 
+		.SCEN(BtnD_pulse), .MCEN( ), .CCEN( ));
 	
-	// disable mamory ports
-	assign {MemOE, MemWR, RamCS, QuadSpiFlashCS} = 4'b1111;
+	ee354_debouncer #(.N_dc(28)) button_L 
+        (.CLK(sys_clk), .RESET(Reset), .PB(BtnL), .DPB( ), 
+		.SCEN(BtnL_pulse), .MCEN( ), .CCEN( ));
+
+	ee354_debouncer #(.N_dc(28)) button_R
+        (.CLK(sys_clk), .RESET(Reset), .PB(BtnR), .DPB( ), 
+		.SCEN(BtnR_pulse), .MCEN( ), .CCEN( ));
+
+	game_array game_space(
+		.Clk(game_clk), 
+		.Ack(Start_Ack), 
+		.Start(Start_Ack), 
+		.Reset(Reset), 
+		.bottom_flag(bottom_flag), 
+		.top_flag(top_flag), 
+		.x1(x1),
+		.y1(y1),
+		.x2(x2),
+		.y2(y2),
+		.x3(x3),
+		.y3(y3),
+		.x4(x4),
+		.y4(y4),
+		.gen_flag(gen_flag), 
+		.arr(arr),  
+		.score(score)
+	);
+
+	block_gen block(
+		.Clk(game_clk), 
+		.Ack(Start_Ack), 
+		.Reset(Reset), 
+		.gen_flag(gen_flag), 
+		.SCEN_U(BtnU_pulse),
+		.SCEN_D(BtnD_pulse), 
+		.SCEN_L(BtnL_pulse), 
+		.SCEN_R(BtnR_pulse),
+		.arr(arr), 
+		.bottom_flag(bottom_flag), 
+		.top_flag(top_flag), 
+		.x1(x1),
+		.y1(y1),
+		.x2(x2),
+		.y2(y2),
+		.x3(x3),
+		.y3(y3),
+		.x4(x4),
+		.y4(y4));
+
 
 endmodule
